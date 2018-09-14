70f2dcdebc0cc2d696eb972c2185560a228ea6a1
Mikhail <madion86@yandex.ru>
2018-09-14 21:34:04 +0300

    main page makeup started

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,50 @@
+import './Content.css';
+
+import React, {Component} from 'react';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Container
+} from 'reactstrap';
+
+import Comments from '../../data/comments';
+
+export default class Content extends Component {
+  render() {
+
+    const comments = Comments.map(item => 
+    <div class="comment">
+    <div className="comment__text">
+    {item.text}
+    </div>
+    <div className="comment__name">
+    {item.userName}
+    </div>
+    </div>);
+    return (
+      <main>
+        <Container>
+          <div className="chat-wrapper">
+            {comments}
+          </div >
+          <Form>
+            <div class="name-input-group">
+              <FormGroup>
+                <Label for="userName" hidden>Name</Label>
+                <Input type="text" name="name" id="userName" placeholder="Enter your name"/>
+              </FormGroup>
+              <Button>Submit</Button>
+            </div>
+            <FormGroup>
+              <Label for="commentInput" hidden>Comment</Label>
+              <Input type="textarea" name="text" id="commentInput" placeholder="Your comment"/>
+            </FormGroup>
+          </Form>
+        </Container>
+      </main >
+    );
+  }
+}
---
+++ src/data/comments.js
@@ -0,0 +1,19 @@
+const Comments = [
+  {
+    text: 'My awesome comment',
+    userName: 'John',
+    time: '9/14/2018'
+  },
+  {
+    text: 'Another awesome comment',
+    userName: 'Mike',
+    time: '8/14/2018'
+  },
+  {
+    text: 'This chat is working!',
+    userName: 'Cool guy',
+    time: '7/14/2018'
+  }
+]
+
+export default Comments;
