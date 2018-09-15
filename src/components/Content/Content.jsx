3c1ad4f50e5403009f127cb25cc87436fb07a4ba
Shadow <madion86@yandex.ru>
2018-09-15 03:36:04 +0300

    interraction with api is in progress

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -10,25 +10,44 @@ import {
   Container
 } from 'reactstrap';
 
-import Comments from '../../data/comments';
+//import Comments from '../../data/comments';
 
 export default class Content extends Component {
-  render() {
 
-    const comments = Comments.map(item => 
-    <div class="comment">
-    <div className="comment__text">
-    {item.text}
-    </div>
-    <div className="comment__name">
-    {item.userName}
-    </div>
+  async getComments() {
+    /*
+    fetch('http://localhost:3000/comments').then(res => {
+      //console.log(res);
+      res.json().then(res => {
+        console.log(res.body);
+        return res.body;
+      })
+    })*/
+    let res = await fetch('http://localhost:3000/comments');
+    let comments = (await res.json()).body;
+    console.log(comments);
+    return Array.from(comments);
+  }
+
+  async render() {
+
+    let comments = await this.getComments();
+
+    console.log(comments);
+
+    const commentsSection = comments.map(item => <div class="comment">
+      <div className="comment__text">
+        {item.text}
+      </div>
+      <div className="comment__name">
+        {item.userName}
+      </div>
     </div>);
     return (
       <main>
         <Container>
           <div className="chat-wrapper">
-            {comments}
+            {commentsSection}
           </div >
           <Form>
             <div class="name-input-group">
@@ -40,7 +59,11 @@ export default class Content extends Component {
             </div>
             <FormGroup>
               <Label for="commentInput" hidden>Comment</Label>
-              <Input type="textarea" name="text" id="commentInput" placeholder="Your comment"/>
+              <Input
+                type="textarea"
+                name="text"
+                id="commentInput"
+                placeholder="Your comment"/>
             </FormGroup>
           </Form>
         </Container>
